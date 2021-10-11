9271012cdf179e54df7a29655e6798cddac11832
Sol Kim <cjzl123475@gmail.com>
2021-10-11 20:22:28 +0900

    REST API CRUD Initialization ( c.21 using Koa )

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,98 @@
+const { post } = require(".");
+
+let postId = 1;
+const posts = [
+    {
+        id: 1,
+        title: 'title',
+        body: 'body',
+    },
+];
+
+//Write a post
+//POST /api/posts
+//{title, body}
+exports.write = ctx => {
+    const { title, body } = ctx.request.body;
+    postId += 1;
+    const post = { id: postId, title, body };
+    posts.push(post);
+    ctx.body = post;
+};
+
+//Inquire post list
+//GET /api/posts
+exports.list = ctx => {
+    ctx.body = posts;
+};
+
+//Inquire a post
+//GET /api/posts/:id
+exports.read = ctx => {
+    const { id } = ctx.params;
+    const post = posts.find(p => p.id.toString() === id);
+    if (!post) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '404\npost not found'
+        };
+        return;
+    }
+    ctx.body = post;
+};
+
+//Delete a post
+//DELETE /api/posts:id
+exports.remove = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '404\npost not found'
+        };
+        return;
+    }
+    posts.splice(index, 1);
+    ctx.status = 204; // No Content
+};
+
+//Replace a post
+//PUT /api/posts/:id
+//{title, body}
+exports.replace = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id );
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '404\npost not found'
+        };
+        return;
+    }
+    posts[index] = {
+        id,
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
+
+//Update a post
+//PATCH /api/posts/:id
+//{title, body}
+exports.update = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id );
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '404\npost not found'
+        }
+        return;
+    }
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
