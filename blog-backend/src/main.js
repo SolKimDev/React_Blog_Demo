25ebb0f9967f36ccc2fe774680ad220f9beb07c1
Sol Kim <cjzl123475@gmail.com>
2021-10-22 17:42:34 +0900

    Project finished with build. Check '.memo' for further process

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -30,6 +33,16 @@ app.use(bodyParser());
 app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  //404 + Address starts with not /api
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0 ) {
+    //return index.html
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 //if port not assigned, use 4000
 const port = PORT || 4000;
