b97bbcb275736b1f6d943b7a1a6dc04bffcc1024
Sol Kim <cjzl123475@gmail.com>
2021-10-22 17:29:51 +0900

    Saving Before Build ( 27.4 )

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -3,10 +3,14 @@ import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/writer/EditorContainer';
 import TagBoxContainer from '../containers/writer/TagBoxContainer';
 import WriteActionButtonsContainer from '../containers/writer/WriteActionButtonsContainer';
+import { Helmet } from 'react-helmet-async';
 
 const WritePage = () => {
     return (
         <Responsive>
+            <Helmet>
+                <title>글 작성하기 - REACTERS</title>
+            </Helmet>
             <EditorContainer />
             <TagBoxContainer />
             <WriteActionButtonsContainer />
